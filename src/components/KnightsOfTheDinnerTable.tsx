095a98c95039a4a95ce87f05e86f5a69378ee790
Mohammad <kurtular.vadisi@gmail.com>
2021-05-13 21:24:33 +0200

    Create KnightsOfTheDinnerTable

---
+++ src/components/KnightsOfTheDinnerTable.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+const input = {
+  Alice: {
+    Bob: 2,
+    Carol: 26,
+    David: -82,
+    Eric: -75,
+    Frank: 42,
+    George: 38,
+    Mallory: 39,
+  },
+  Bob: {
+    Alice: 40,
+    Carol: -61,
+    David: -15,
+    Eric: 63,
+    Frank: 41,
+    George: 30,
+    Mallory: 87,
+  },
+  Carol: {
+    Alice: -35,
+    Bob: -99,
+    David: -51,
+    Eric: 95,
+    Frank: 90,
+    George: -16,
+    Mallory: 94,
+  },
+  David: {
+    Alice: 36,
+    Bob: -18,
+    Carol: -65,
+    Eric: -18,
+    Frank: -22,
+    George: 2,
+    Mallory: 42,
+  },
+  Eric: {
+    Alice: -65,
+    Bob: 24,
+    Carol: 100,
+    David: 51,
+    Frank: 21,
+    George: 55,
+    Mallory: -44,
+  },
+  Frank: {
+    Alice: -48,
+    Bob: 91,
+    Carol: 8,
+    David: -66,
+    Eric: 97,
+    George: -9,
+    Mallory: -92,
+  },
+  George: {
+    Alice: -44,
+    Bob: -25,
+    Carol: 17,
+    David: 92,
+    Eric: -92,
+    Frank: 18,
+    Mallory: 97,
+  },
+  Mallory: {
+    Alice: 92,
+    Bob: -96,
+    Carol: -51,
+    David: -81,
+    Eric: 31,
+    Frank: -73,
+    George: -89,
+  },
+};
+
+const KnightsOfTheDinnerTable = () => {
+  return <div></div>;
+};
+
+export default KnightsOfTheDinnerTable;
