9037bf9c3c2bdfada0461fcc3133d9f8e7ee46cf
Mohammad <kurtular.vadisi@gmail.com>
2021-05-13 21:24:14 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import KnightsOfTheDinnerTable from "./components/KnightsOfTheDinnerTable";
+
+const App = () => {
+  return (
+    <div className="App">
+      <KnightsOfTheDinnerTable />
+    </div>
+  );
+};
+
+export default App;
