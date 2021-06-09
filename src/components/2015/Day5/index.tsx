2addb2826957a5901d9512cec58acd0d3683d8a5
Mohammad <kurtular.vadisi@gmail.com>
2021-06-09 16:11:30 +0200

    create Day5

---
+++ src/components/2015/Day5/index.tsx
@@ -0,0 +1,21 @@
+import input from "./input";
+
+const Day5 = () => {
+  const solution = getSolution();
+  return (
+    <div>
+      <h4>day5: Doesn't He Have Intern-Elves For This?</h4>
+      <p>How many strings are nice?:{solution}</p>
+    </div>
+  );
+};
+
+export default Day5;
+
+const getSolution = () => {
+  const result = 0;
+    input.split("\n").forEach((word) => {
+      
+  });
+  return result;
+};
--- src/components/2015/index.tsx
+++ src/components/2015/index.tsx
@@ -2,6 +2,7 @@ import Day1 from "./Day1";
 import Day2 from "./Day2";
 import Day3 from "./Day3";
 import Day4 from "./Day4";
+import Day5 from "./Day5";
 
 const Year2015 = () => {
   return (
@@ -11,6 +12,7 @@ const Year2015 = () => {
       <Day2 />
       <Day3 />
       <Day4 />
+      <Day5 />
       <hr />
     </div>
   );
