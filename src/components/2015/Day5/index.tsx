b5f17a3255f4c406f262efccd866d52438fa6d33
Mohammad <kurtular.vadisi@gmail.com>
2021-06-11 18:12:39 +0200

    Solve day 5 of 2015

--- src/components/2015/Day5/index.tsx
+++ src/components/2015/Day5/index.tsx
@@ -1,21 +1,43 @@
 import input from "./input";
 
 const Day5 = () => {
-  const solution = getSolution();
   return (
     <div>
       <h4>day5: Doesn't He Have Intern-Elves For This?</h4>
-      <p>How many strings are nice?:{solution}</p>
+      <p>part1: How many strings are nice?:{getFirstPartSolution(input)}</p>
+      <p>part2: How many strings are nice?:{getSecondPartSolution(input)}</p>
     </div>
   );
 };
 
 export default Day5;
 
-const getSolution = () => {
-  const result = 0;
-    input.split("\n").forEach((word) => {
-      
+const getFirstPartSolution = (input: string) => {
+  let result = 0;
+  input.split("\n").forEach((word: string) => {
+    const isContainsDisallowedSubString = /ab|cd|pq|xy/g.test(word);
+    const isContainsDubbelLetter =
+      /(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz){1,}/g.test(
+        word
+      );
+    const vowels = word.match(/[aeiou]/g);
+    const isContainsAtLeastThreeVowels = vowels?.length && vowels.length > 2;
+
+    isContainsAtLeastThreeVowels &&
+      !isContainsDisallowedSubString &&
+      isContainsDubbelLetter &&
+      result++;
+  });
+  return result;
+};
+
+const getSecondPartSolution = (input: string) => {
+  let result = 0;
+  input.split("\n").forEach((word: string) => {
+    const isContainsXYXPattern = /(.).\1/g.test(word);
+    const isContainsDubbelSubString = /(..).*\1/gm.test(word);
+
+    isContainsXYXPattern && isContainsDubbelSubString && result++;
   });
   return result;
 };
